8b6aa2b497f14b8d0ed39e3e68912937a24db87a
RajshreeS2024 <rajshree.sengupta@gmail.com>
2024-10-28 15:27:00 +0530

    Update StringCalculator.tests.cpp

--- StringCalculator.tests.cpp
+++ StringCalculator.tests.cpp
@@ -4,7 +4,7 @@
 //TEST(TestSuireName, TestCaseName)
 TEST(string_calculator_add,when_passed_a_single_number_returns_0_Empty){
 //Arrange
-string input=""
+string input="";
 int expectedValue=0;
 
 //Act
