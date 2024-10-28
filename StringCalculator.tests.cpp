b15c4625776c5e3b55047e568e23b8f242de5357
RajshreeS2024 <rajshree.sengupta@gmail.com>
2024-10-28 14:43:56 +0530

    Update StringCalculator.tests.cpp

--- StringCalculator.tests.cpp
+++ StringCalculator.tests.cpp
@@ -1,1 +1,15 @@
+#include "StringCalculator.cpp"
+#include <gtest/gtest.h>
 
+//TEST(TestSuireName, TestCaseName)
+TEST(string_calculator_add,when_passed_a_single_number_returns_0_Empty){
+//Arrange
+string input=""
+int expectedValue=0;
+
+//Act
+int actualValue=objUnderTest.Add(input);
+
+//Assert
+ASSET_EQ(expectedValue, actualValue);
+}
